4bd3acd618d9b38588bc3cb37d47c15f9ecf51ce
LNickelsburg <l.nickelsburg@gmail.com>
2024-12-10 09:16:43 -0500

    fixed regional and user datasets

--- DataCollection/data_wrangling.py
+++ DataCollection/data_wrangling.py
@@ -22,20 +22,22 @@ def load_data(clean):
 
 def clean_regional(data):
     data.rename(columns={'date': 'chart_date'}, inplace=True)
-    data.drop(columns=['rank'], inplace=True)
     return data
 
 def clean_user(data):
     data.rename(columns={'category': 'user_metric'}, inplace=True)
+
     return data
 
 def clean_info(data):
     data.rename(columns={'date': 'release_date'}, inplace=True)
     data.drop(columns=['name'], inplace=True)
+    data.drop_duplicates(subset=['uri'], inplace=True)
     return data
 
 def clean_features(data):
     data.rename(columns={'id': 'uri'}, inplace=True)
+    data.drop_duplicates(subset=['uri'], inplace=True)
     return data
 
 def clean_geo(data):
@@ -46,14 +48,29 @@ def clean_geo(data):
 ### MERGE DATA ###
 
 def get_regional(regional, info, features):
-    regional = regional.merge(info, on='uri', how='inner')
-    regional = regional.merge(features, on='uri', how='inner')
-    return regional
+    
+    regional_info = regional.merge(info, on='uri', how='left')
+    regional_info = regional_info.merge(features, on='uri', how='left')
+    regional_info.drop_duplicates(subset=['chart_date', 'region', 'uri'], inplace=True)
+
+
+    regional_features = regional.merge(features, on='uri', how='inner')
+    regional_features = regional_features.merge(info, on='uri', how='left')
+    regional_features.drop_duplicates(subset=['chart_date', 'region', 'uri'], inplace=True)
+
+    return regional_info, regional_features
 
 def get_user(user, info, features):
-    user = user.merge(info, on='uri', how='inner')
-    user = user.merge(features, on='uri', how='inner')
-    return user
+
+    user_info = user.merge(info, on='uri', how='left')
+    user_info = user_info.merge(features, on='uri', how='left')
+    user_info.drop_duplicates(subset=['user_metric', 'uri'], inplace=True)
+
+    user_features = user.merge(features, on='uri', how='inner')
+    user_features = user_features.merge(info, on='uri', how='left')
+    user_features.drop_duplicates(subset=['user_metric', 'uri'], inplace=True)
+
+    return user_info, user_features
 
 def results_to_csv(df, path):
     path = f"C:/Users/lnick/OneDrive/Desktop/Fall 2024/archives as data/final project/DataCollection/{path}"
@@ -61,31 +78,38 @@ def results_to_csv(df, path):
         df.to_csv(path, index=False)
 
 def merge_data():
+    print('Loading data...')
     regional_data, user_data, track_info, track_features, countries_geo = load_data(clean=True)
 
-    regional_data = get_regional(regional_data, track_info, track_features)
-    user_data = get_user(user_data, track_info, track_features)
+    print('Merging data...')
+    regional_info, regional_features = get_regional(regional_data, track_info, track_features)
+    user_info, user_features = get_user(user_data, track_info, track_features)
 
-    results_to_csv(regional_data, 'data/regional_data.csv')
-    results_to_csv(user_data, 'data/user_data.csv')
+    print('Saving data...')
+    results_to_csv(regional_info, 'data/regional_info.csv')
+    results_to_csv(regional_features, 'data/regional_features.csv')
+    results_to_csv(user_info, 'data/user_info.csv')
+    results_to_csv(user_features, 'data/user_features.csv')
+    
 
 
 ### ACCESS DATA ###
 
 def get_datasets():
-    regional_data = pd.read_csv('DataCollection/data/regional_data.csv')
-    user_data = pd.read_csv('DataCollection/data/user_data.csv')
+    regional_info = pd.read_csv('DataCollection/data/regional_info.csv')
+    regional_features = pd.read_csv('DataCollection/data/regional_features.csv')
+    user_info = pd.read_csv('DataCollection/data/user_info.csv')
+    user_features = pd.read_csv('DataCollection/data/user_features.csv')
+
     countries_geo = gpd.read_file('DataCollection/data/raw/geospatial/countries.geo.json')
     countries_geo = clean_geo(countries_geo)
-    return regional_data, user_data, countries_geo
+
+    return regional_info, regional_features, user_info, user_features, countries_geo
 
 
 
 if __name__ == '__main__':
-    #merge_data()
-
-    regional_data, user_data, countries_geo = get_datasets()
-    print(regional_data.columns)
+    merge_data()
 
 
     
